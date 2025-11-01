9769268e27bfad7fdb975e3081f2295fad20b916
Nahli <kopislur64@gmail.com>
2025-11-01 18:28:27 +0700

    ERR_HTTP_RESPONSE_CODE_FAILURE 404 (Not Found)

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    {
+      name: 'rewrite-root-to-public-index',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url === '/' || req.url === '') req.url = '/index.html';
+          next();
+        });
+      }
+    }
+  ]
+});
